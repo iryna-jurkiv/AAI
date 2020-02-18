ee7a8fb7e56ffaeed7499fa2880395400c633c6a
Aaron Diable <aaron.diable@gmail.com>
2020-02-18 11:10:39 +0000

    Route changes

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-const express = require('express')
-const path = require('path')
-const {client} = require('../db/db_config');
+const express = require('express');
+const {client} = require('./db/db_config');
 
 //Initialises app
 const app = express();
 
 
 //Load View Engine
-app.set('views',path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
-var usersRouter = require('./routes/users') //Path
+
+
+const usersRouter = require('./routes/users'); //Path
+const apiRouter = require('./routes/api'); //Path
 
 app.use('/users', usersRouter); // sets up the route for the user page, this links the variable to the route's folder.
+app.use('/api', apiRouter); // sets up the route for the user page, this links the variable to the route's folder.
 
 
 
 
 //Start Server
-app.listen('3000',function(){
+app.listen('3000',() => {
   console.log('Server started on port 3000')
 });

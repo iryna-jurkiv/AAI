17b9f598a3bd33fb698c1c0899e39a33466d1d8c
Aaron Diable <aaron.diable@gmail.com>
2020-02-20 19:46:46 +0000

    Styling

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static('public'));
 
 const employeesRouter = require('./routes/employees'); //Path
 const usersRouter = require('./routes/users'); //Path

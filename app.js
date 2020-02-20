da41d50068062ed8e5b0b214c44ab6fe99ab7ab5
Iryna Jurkiv <iryna.jurkiv@gmail.com>
2020-02-20 16:14:33 +0000

    Employees now receive email with their randomly generated password and can login and see thie rprofile

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-
-//Initialises app
+const nodemailer = require('nodemailer')
 const app = express();
-
 require('dotenv').config(); // Sets ENV configs for DB access and other global configs
+
+// let transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth:{
+//     user:process.env.EMAIL,
+//     pass:process.env.PASSWORD
+//   }
+// });
+//
+// let mailOptions = {
+//   from: 'aaiteam20@gmail.com',
+//   to:'iryna.jurkiv@gmail.com',
+//   bcc: 'aaiteam20@gmail.com',
+//   subject: 'Testing',
+//   text: 'Test'
+// };
+//
+// transporter.sendMail(mailOptions, function(err,data){
+//   if (err) {
+//     console.log('Error occurs', err);
+//   } else {
+//     console.log('Email sent')
+//   }
+// });
 
 //Load View Engine
 app.set('view engine', 'ejs');
@@ -12,10 +34,11 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cookieParser());
 
-
+const employeesRouter = require('./routes/employees'); //Path
 const usersRouter = require('./routes/users'); //Path
 const apiRouter = require('./routes/api'); //Path
 
+app.use('/employees', employeesRouter); // sets up the route for the user page, this links the variable to the route's folder.
 app.use('/users', usersRouter); // sets up the route for the user page, this links the variable to the route's folder.
 app.use('/api', apiRouter); // sets up the route for the user page, this links the variable to the route's folder.
 
--- routes/api.js
+++ routes/api.js
@@ -3,20 +3,57 @@ const router = express.Router();
 const {client} = require('../db/db_config');
 const bcrypt = require('bcrypt');
 const cookies = require('cookie-parser');
+const nodemailer = require('nodemailer')
+require('dotenv').config(); // Sets ENV configs for DB access and other global configs
 let SALT = 10
 
 
 router.post('/addemployee', async (req, res) => {
     const {firstname, lastname, jobtitle, employeenumber, email } = req.body;
     try{
-       await client.query(`INSERT INTO employees (first_name, last_name, job_title, employee_number, email) 
+       await client.query(`INSERT INTO employees (first_name, last_name, job_title, employee_number, email)
         VALUES ('${firstname}','${lastname}','${jobtitle}','${employeenumber}','${email}')`);
+      let employee = await client.query(`SELECT * FROM employees WHERE employee_number = '${employeenumber}'`);
+      console.log(employee.rows[0])
+        let transporter = nodemailer.createTransport({
+          service: 'gmail',
+          auth:{
+            user:process.env.EMAIL,
+            pass:process.env.PASSWORD
+          }
+        });
+
+
+        let mailOptions = {
+          from: 'aaiteam20@gmail.com',
+          to: req.body.email,
+          bcc: 'aaiteam20@gmail.com',
+          subject: 'Login Details',
+          html: '<!DOCTYPE html>'+
+                '<html><head><title>AAI Login Details</title>'+
+                '</head><body><div>'+
+                '<p>Dear Candidate </p>'+
+                '<p>Welcome to AAI User Access Management System. Please go to http://localhost:3000/users/signin and use the following email and temporary password to sign in and view your profile </p>'
+                + employee.rows[0]['email'] +
+                '<br>'
+                + employee.rows[0]['password']+
+                '<p>Kind Regards, </p>'+
+                '<p>The AAI Team</p>'+
+                '</div></body></html>'
+              };
+        transporter.sendMail(mailOptions, function(err,data){
+          if (err) {
+            console.log('Error occurs', err);
+          } else {
+            console.log('Email sent')
+          }
+        });
         res.redirect('/users/addemployee')
     }catch(err){
         console.log(err)
        res.json({
            message: 'Error',
-           err     
+           err
         })
     }
 })
@@ -60,6 +97,34 @@ router.post('/signin',async (req, res) => {
         })
     }
 });
+
+router.post('/employeessignin',async (req, res) => {
+    const {email, password } = req.body;
+    try{
+    const foundEmployee = await client.query(`SELECT * FROM employees WHERE email = '${email}'`);
+    if (password === foundEmployee.rows[0]['password']) {
+      res.cookie('employeefirstname', foundEmployee.rows[0]['first_name'])
+        res.redirect('/employees')
+    } else {
+        res.json ({
+            message: 'Incorrect Password',
+            url:'http://localhost:3000/employees/signin'
+         })
+        }
+
+
+    }catch(err){
+        res.json({
+            message: 'Error Logging In',
+            err
+        })
+    }
+});
+
+router.post('/employeessignout', async (req, res) => {
+   res.clearCookie('employeefirstname')
+   res.redirect('/employees/employeessignin')
+})
 
 router.post('/signout', async (req, res) => {
    res.clearCookie('username')
